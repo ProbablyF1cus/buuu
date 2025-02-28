2578f170dbad17f620c84f275afb14d684fe0f5b
Vlad <Vladvil08@yandex.ru>
2025-02-28 17:53:27 +0300

    du

--- вф.py
+++ вф.py
@@ -2,6 +2,9 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def title():
+    return 'Миссия Колонизация Марса'
 
 @app.route('/image_mars')
 def return_sample_page():
@@ -18,22 +21,15 @@ def return_sample_page():
                     <p> Вот она какая, красная планета.</p>
                   </body>
                 </html>'''
-
-
-@app.route('/')
-def title():
-    return 'Миссия Колонизация Марса'
-
-
 @app.route('/index')
 def index():
     return "И на Марсе будут яблони цвести!"
-
-
 @app.route('/promotion')
 def promotion():
     return "Человечество вырастает из детства.<br>Человечеству мала одна планета.<br>Мы сделаем обитаемыми безжизненные пока планеты.<br>И начнем с Марса!<br>Присоединяйся!"
 
 
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
+
