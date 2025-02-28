69d8e6883c8940e5510b42403aec2324fba72889
Vlad <Vladvil08@yandex.ru>
2025-02-28 17:33:36 +0300

    du

--- вф.py
+++ вф.py
@@ -1,8 +1,21 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-
+@app.route('/image_mars')
+def return_sample_page():
+    return """<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='MARS.png')}"
+            alt="здесь должна была быть картинка, но что-то пошло не так :("
+                    <p> Вот она какая, красная планета <p>
+                  </body>
+                </html>"""
 @app.route('/')
 def title():
     return 'Миссия Колонизация Марса'
