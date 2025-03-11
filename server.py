e6a5f036b65227db8d1a1908bcad17e0a24bc6f3
Vlad <Vladvil08@yandex.ru>
2025-03-11 18:02:49 +0300

    Тренировки в полёте

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index(word):
     return render_template('base.html', title=word)
 
+@app.route('/training/<porf>')
+def index(word):
+    return render_template('training.html', prof=prof)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
